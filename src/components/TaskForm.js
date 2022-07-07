42766d3696cadda16b3d5a730bd13c97ee1955fb
Humberto López <g.lops.h1@gmail.com>
2022-07-07 11:24:24 +0200

    Succeded tutorial

--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -45,10 +45,13 @@ function TaskForm() {
   }, []);
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input name="title" type="text" placeholder="title" onChange={handleChange} value={task.title}/>
-      <textarea name="description" placeholder="description" onChange={handleChange} value={task.description}></textarea>
-      <button>Save</button>
+    <form onSubmit={handleSubmit} className="bg-zinc-800 max-w-sm p-4">
+      <label htmlFor="title" className="block text-xs font-bold mb-2">Task:</label>
+      <input name="title" type="text" placeholder="title" onChange={handleChange} value={task.title} className="w-full p-2 rounded-md bg-zinc-600 mb-2"/>
+
+      <label className="block text-xs font-bold mb-2">Description:</label>
+      <textarea name="description" placeholder="description" onChange={handleChange} value={task.description} className="w-full p-2 rounded-md bg-zinc-600 mb-2"></textarea>
+      <button className="bg-indigo-600 px-2 py-1">Save</button>
     </form>
   )
 }
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,30 +1,37 @@
 import { useSelector, useDispatch } from "react-redux";
-import {deleteTask} from '../features/tasks/taskSlice'
-import {Link} from 'react-router-dom'
+import { deleteTask } from "../features/tasks/taskSlice";
+import { Link } from "react-router-dom";
 
 function TaskList() {
-  const tasks = useSelector(state => state.tasks);
+  const tasks = useSelector((state) => state.tasks);
 
   const dispatch = useDispatch();
 
   const handleDelete = (id) => {
-    dispatch(deleteTask(id))
-  }
+    dispatch(deleteTask(id));
+  };
 
   return (
-    <div>
-      <header>
+    <div className="w-4/6">
+      <header className="flex justify-between items-center py-4">
         <h1>Tasks {tasks.length}</h1>
-        <Link to='/create-task'>Create task</Link>
+        <Link to="/create-task" className="bg-indigo-600 px-2 py-1 rounded-sm text-sm">Create task</Link>
       </header>
-      {tasks.map(task => (
-        <div key={task.id}>
-          <h3>{task.title}</h3>
+      <div className="grid grid-cols-3 gap-4">
+      {tasks.map((task) => (
+        <div key={task.id} className="bg-neutral-800 p-4 rounded-md">
+          <header className="flex justify-between">
+            <h3>{task.title}</h3>
+            <div className="flex gap-x-2">
+              <Link to={`/edit-task/${task.id}`} className="bg-zinc-600 px-2 py-1 text-xs rounded-md">Edit</Link>
+              <button onClick={() => {handleDelete(task.id)}} className="bg-red-500 px-2 py-1 text-xs rounded-md">Delete</button>
+            </div>
+          </header>
           <p>{task.description}</p>
-          <button onClick={() => {handleDelete(task.id)}}>Delete</button>
-          <Link to={`/edit-task/${task.id}`}>Edit</Link>
         </div>
       ))}
+      </div>
+
     </div>
   );
 }
